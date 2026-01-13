a203242eb072bae337544a2bbfb7e0bc5d787a47
= <=>
2026-01-13 14:34:03 +0100

    pantalla inicio

--- proyecto/resources/views/inicio.blade.php
+++ proyecto/resources/views/inicio.blade.php
@@ -3,13 +3,23 @@
 
 @section('contenido')
 
-<x-sidebar nombre="paco" rol="profesor" esProfesor="false"></x-sidebar>
+<x-sidebar nombre="{{$nombreUsuario}}" rol="{{$rol}}" es-profesor="{{$esProfesor}}"></x-sidebar>
 
-<x-contenido-principal titulo="aa">
-    <h1>si</h1>
-    <x-mensaje id-mensaje="22" usuario="paco" asignatura="bd" fecha="hoy">
-        soy paco
+<x-contenido-principal titulo="Inicio">
+
+    @foreach($mensajes as $mensaje)
+    <x-mensaje 
+        idMensaje="{{$mensaje->id}}"
+        asignatura="nombreasignatura"
+        fecha="2348/87234/342"
+        usuario="paco"
+        :haSidoModerado="true"
+    >
+        {{$mensaje->contenido}}
     </x-mensaje>
+    @endforeach
+
+
 </x-contenido-principal>
 
 @endsection
--- proyecto/routes/web.php
+++ proyecto/routes/web.php
@@ -1,7 +1,23 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Models\User;
+use App\Models\Mensaje;
+use App\Models\EstadosMensaje;
 
 Route::get('/', function () {
-    return view('inicio');
+
+    $usuarioPrueba = new User("a", "b", "c", "d");
+    $mensajes = [
+        new Mensaje("hola que tal", 2, 2, EstadosMensaje::PUBLICADO, 1, 2),
+        new Mensaje("pruebaaa", 2, 2, EstadosMensaje::PUBLICADO, 1, 2),
+        // (string $contenido, int $idAsignatura, int $idUsuario, string $estadoMensaje, int $timestampCreacion, ?int $id )
+    ];
+    $datos = [
+        "nombreUsuario" => "paco",
+        "rol" => "si",
+        "esProfesor" => false,
+        "mensajes" => $mensajes,
+    ];
+    return view('inicio', $datos);
 });
