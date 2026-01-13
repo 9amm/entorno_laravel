50abe4cabf69cedf4df570c173ba6bfaeb0ff0b2
= <=>
2026-01-13 12:16:33 +0100

    componente topbar

--- proyecto/resources/views/inicio.blade.php
+++ proyecto/resources/views/inicio.blade.php
@@ -4,9 +4,13 @@
 @section('contenido')
 
 
-<x-mensaje id-mensaje="22" usuario="paco" asignatura="bd" fecha="hoy">
-    esto es contenido
-</x-mensaje>
+    <x-mensaje id-mensaje="22" usuario="paco" asignatura="bd" fecha="hoy">
+        esto es contenido
+    </x-mensaje>
+
+    <x-topbar>
+        Inicio
+    </x-topbar>
 
 
 @endsection
